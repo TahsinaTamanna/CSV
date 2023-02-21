4eb39085b94bdbeb73c8ab37bd11b24e9ba6e0d4
ttamanna <t.tamanna312@gmail.com>
2023-02-21 14:48:49 -0600

    Updated exceptions file

--- exceptions.py
+++ exceptions.py
@@ -2,15 +2,28 @@
 payrate = 10.0
 paycheck = 0
 
+'''
 try:
     answer = float(input("How many hours did you work?"))
 
-except:
-    print("There was an error")
+except ValueError as err:
+    print(f"There was an error. The error code is {err}")
+    #pass
 
 else:
     paycheck = answer * payrate
-       
-    
+          
 print(f"Your paycheck is ${paycheck:,.2f}")
 
+'''
+while True:
+    try:
+        answer = float(input("How many hours did you work?"))
+        paycheck = answer * payrate
+        print(f"Your paycheck is ${paycheck:,.2f}")
+        break
+
+    except ValueError as err:
+        print(f"There was an error. The error code is {err}")
+    
+print("It is done")
